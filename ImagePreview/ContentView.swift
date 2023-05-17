09cc7578e5856ee3ca7054def9f7e72a9abc104b
evan <98708887@qq.com>
2023-05-17 10:18:00 +0800

    Initial Commit

---
+++ ImagePreview/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ImagePreview
+//
+//  Created by evan on 2023/5/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ImagePreview/ImagePreviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImagePreviewApp.swift
+//  ImagePreview
+//
+//  Created by evan on 2023/5/17.
+//
+
+import SwiftUI
+
+@main
+struct ImagePreviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
